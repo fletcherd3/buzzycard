2e8b7932943a734e6a41bdadbfe90ca227a3b849
fletcher_dick <fletcherjdick@gmail.com>
2020-06-11 20:24:05 +1200

    Added in test user schema for testing DB

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+    position: String,
+    name: String,
+    company: String,
+    company2: String,
+    date: Date
 });
 
 const User = mongoose.model('User', UserSchema);
