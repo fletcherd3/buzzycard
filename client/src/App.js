7eb27727cfd54d2954c7406f6f197446d30dc6c8
fletcher_dick <fletcherjdick@gmail.com>
2020-06-11 19:35:37 +1200

    Adding stripped down MERN stack template from Esterling Accime

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Form from './components/Form';
+import DisplayUsers from './components/DisplayUsers';
+import { BusinessCard } from './components/BusinessCard';
+import { NotFound } from './components/notfound';
+import axios from 'axios';
+import './App.css';
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = () => {
+    this.fetchUsers();
+  };
+
+  fetchUsers = () => {
+    // axios.get('/users')
+    //   .then((response) => {
+    //     const { users } = response.data;
+    //     this.setState({ users: [...this.state.users, ...users] })
+    //   })
+    //   .catch(() => alert('Error fetching new users'));
+  };
+
+
+  addUser = ({ name, position, company, company2 }) => {
+    this.setState({
+      users: [...this.state.users, { name, position, company, company2 }]
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+              <Switch>
+                <Route path="/" exact
+                render={(props) => <Form {...props} addUser={this.addUser} />}
+                />
+                <Route path="/user" exact
+                render={(props) => <BusinessCard {...props} />}
+                />
+                <Route path="/table" exact
+                render={(props) => <DisplayUsers {...props} users={this.state.users} />}
+                />
+                <Route path ="*" exact component={NotFound} />
+              </Switch>
+        </BrowserRouter>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/notfound.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const NotFound = () => (
+  <React.Fragment>
+      <h1>Oops! That page can’t be found.</h1>
+      <p>
+              It looks like nothing was found at this location.
+      </p>
+  </React.Fragment>
+);
