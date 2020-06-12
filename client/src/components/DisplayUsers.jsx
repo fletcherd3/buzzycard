100c16f9141650acd122e66e1c71d6d954007ba4
fletcher_dick <fletcherjdick@gmail.com>
2020-06-12 21:13:59 +1200

    Put fetch in component Did Mount

--- client/src/components/DisplayUsers.jsx
+++ client/src/components/DisplayUsers.jsx
@@ -11,7 +11,7 @@ class DisplayUser extends Component {
         users: []
       }
 
-    componentWillMount = () => {
+    componentDidMount = () => {
         await this.fetchUsers();
       };
     
