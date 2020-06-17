ded0e06a2138338158ad64d067d354c24827d534
fletcher_dick <fletcherjdick@gmail.com>
2020-06-18 11:17:57 +1200

    Adding link to snapchat

--- client/src/components/buttons/FacebookButton.jsx
+++ client/src/components/buttons/FacebookButton.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Grid, TextField, Button  } from '@material-ui/core';
 import "../../App.css";
+import {Linking} from 'react-native';
 import {ReactComponent as FacebookLogo} from '../../assets/icons/facebook.svg';
 
 
+
+
 class FacebookButton extends React.Component {
+
+  openApp() {
+    Linking.openURL('snapchat://add/fletcher.dick');
+  };
+
   render() {
     return (
         <div className='display'>
-          <Button variant="contained" size="large" color="primary" className='pageButton'>
+          <Button variant="contained" size="large" color="primary" className='pageButton' onClick={this.openApp}>
               <FacebookLogo className='icon'/>
               Facebook
           </Button>
